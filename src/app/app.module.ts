9ec85766122ddb0bc5382e318a6205e92948815c
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-11-11 07:31:23 +0530

    Added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,11 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./routes/home/home.component";
-import { NavbarComponent } from "./components/navbar/navbar.component";
 import { FeedbackComponent } from "./routes/feedback/feedback.component";
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    NavbarComponent,
-    FeedbackComponent,
-  ],
+  declarations: [AppComponent, HomeComponent, FeedbackComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
