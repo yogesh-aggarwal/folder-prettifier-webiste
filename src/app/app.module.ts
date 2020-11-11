d04d262efc9f1c217f104f5dcc77a807b63b29bb
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-11-11 09:16:41 +0530

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,12 @@ import { AppComponent } from "./app.component";
 import { HomeComponent } from "./routes/home/home.component";
 import { FeedbackComponent } from "./routes/feedback/feedback.component";
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { PrivacyComponent } from './routes/privacy/privacy.component';
+import { DownloadComponent } from './routes/download/download.component';
+import { GuideComponent } from './routes/guide/guide.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, FeedbackComponent, NavbarComponent],
+  declarations: [AppComponent, HomeComponent, FeedbackComponent, NavbarComponent, PrivacyComponent, DownloadComponent, GuideComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
